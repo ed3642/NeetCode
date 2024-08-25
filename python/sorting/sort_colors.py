8c0512bd997811b1815e0dd497e21a6a55fccf3b
ed3642 <ed3642dev@gmail.com>
2024-08-24 23:21:07 -0400

    solve

--- python/sorting/sort_colors.py
+++ python/sorting/sort_colors.py
@@ -1,3 +1,4 @@
+# https://leetcode.com/problems/sort-colors
 class Solution:
     def sortColors(self, nums: list[int]) -> None:
         """
@@ -5,22 +6,19 @@ class Solution:
         """
         # dutch national flag problem
         # l and r are where you place the 0's and 2's
-        # i is the elem were inspecting
+        # m is the elem were inspecting
         
         l = 0
         r = len(nums) - 1
-        i = 0
+        m = 0
 
-        while i <= r:
-            if nums[i] == 0:
-                nums[i], nums[l] = nums[l], nums[i]
+        while m <= r:
+            if nums[m] == 0:
+                nums[m], nums[l] = nums[l], nums[m]
                 l += 1
-                i += 1
-            elif nums[i] == 2:
-                nums[i], nums[r] = nums[r], nums[i]
+                m += 1
+            elif nums[m] == 2:
+                nums[m], nums[r] = nums[r], nums[m]
                 r -= 1
             else:
-                i += 1
-
-s = Solution()
-s.sortColors([2,0,2,1,1,0])
+                m += 1
